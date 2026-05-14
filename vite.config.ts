b4e332e9159f3f5ce8f0fa69e75a6a8966ac36e8
Filipe Verrone de Lima <verronefilipe@gmail.com>
2026-05-13 21:50:39 -0300

    fix: vite config to make it render correctly

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,18 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      '@mui/material',
+      '@mui/material/Box',
+      '@mui/material/Stack',
+      '@mui/material/Slider',
+      '@mui/icons-material/VolumeDown',
+      '@mui/icons-material/VolumeUp',
+      '@emotion/react',
+      '@emotion/styled',
+    ],
+  },
   test: {
     globals: true,
     environment: 'jsdom',
